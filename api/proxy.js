4fc0a245509fd1ddf00cb99b9c33cdec4ea23fdf
TENSAEA <tensaeaschalew26@gmail.com>
2025-03-20 20:13:19 +0300

    update

--- api/proxy.js
+++ api/proxy.js
@@ -14,6 +14,12 @@ export default async (req, res) => {
       method: req.method,
       headers: {
         "Content-Type": "application/json",
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+        Accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.5",
+        Referer: "https://ecommercetensae.infinityfreeapp.com/",
       },
       body: req.method === "POST" ? JSON.stringify(req.body) : null,
       agent: new https.Agent({
